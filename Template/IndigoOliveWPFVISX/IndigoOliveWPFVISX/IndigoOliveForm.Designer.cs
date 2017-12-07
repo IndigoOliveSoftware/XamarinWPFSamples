5d1fd3b68611b666999e8258251f17e7515b4de8
DERBY-ACER\Developer <johnrussell8236@yahoo.com>
2017-12-07 17:19:35 -0500

    Adding VISX

---
+++ Template/IndigoOliveWPFVISX/IndigoOliveWPFVISX/IndigoOliveForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace IndigoOliveWPFVISX {
+    partial class IndigoOliveForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnCreateButtonClicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnCancelButtonClicked);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(225, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Android-iOS-UWP-WPF Master Detail PCL";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(192, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Android-iOS-UWP-WPF Empty PCL";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 58);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(254, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Android-iOS-UWP-WPF Master Detail PCL Maps";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 81);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(239, 17);
+            this.radioButton4.TabIndex = 10;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Android-iOS-UWP-WPF Master Detail Shared";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 104);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(206, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Android-iOS-UWP-WPF Empty Shared";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // IndigoOliveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 210);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "IndigoOliveForm";
+            this.Text = "IndigoOliveForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
---
+++ Template/IndigoOliveWPFVISX/IndigoOliveWPFVISX/WizardImplementationClass1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Windows.Forms;
+using EnvDTE;
+using System.IO;
+using EnvDTE100;
+using EnvDTE80;
+
+namespace IndigoOliveWPFVISX {
+    public class WizardImplementation : IWizard {
+        private IndigoOliveForm inputForm;
+        private Dictionary<string, string> _replacementsDictionary = new Dictionary<string, string>();
+        private EnvDTE.DTE _dte = null;
+
+        // This method is called before opening any item that   
+        // has the OpenInEditor attribute.  
+        public void BeforeOpeningFile(ProjectItem projectItem) {
+        }
+
+        public void ProjectFinishedGenerating(Project project) {
+        }
+
+        // This method is only called for item templates,  
+        // not for project templates.  
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem) {
+        }
+
+        // This method is called after the project is created.  
+        public void RunFinished() {
+            string destination = _replacementsDictionary["$destinationdirectory$"];
+            string fileName = _replacementsDictionary["$safeprojectname$"] + ".sln";
+            _dte.Solution.SaveAs(Path.Combine(destination, fileName));
+
+            if (IndigoOliveForm.mg_eSelectedType == IndigoOliveForm.IndigoOliveSelectedType.EMPTY_PCL) {
+                var projectName = $"{_replacementsDictionary["$safeprojectname$"]}";
+                var templateName = "EmptyPCLINDIGOWPF";
+
+                AddProject(destination, projectName, templateName);
+            }
+        }
+
+        private void AddProject(string destination, string projectName, string templateName) {
+            string projectPath = Path.Combine(destination, projectName);
+            string templatePath = ((Solution4)_dte.Solution).GetProjectTemplate(templateName, "CSharp");
+
+            _dte.Solution.AddFromTemplate(templatePath, projectPath, projectName, false);
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams) {
+            /** Store the reference to the environment for later use **/
+            _dte = automationObject as EnvDTE.DTE;
+            _replacementsDictionary = replacementsDictionary;
+
+            try {
+                // Display a form to the user. The form collects   
+                // input for the custom message.  
+                inputForm = new IndigoOliveForm();
+                inputForm.ShowDialog();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,  
+        // not for project templates.  
+        public bool ShouldAddProjectItem(string filePath) {
+            return true;
+        }
+    }
+}
